5a7239fd75d75d1570df7c59d094c3a5d34d1117
Krzysztof Sobolewski <krzysztof.sobolewski@gmail.com>
2025-06-24 22:29:12 +0200

    RE #update

--- soboAppTpl/src/commonMain/kotlin/com/krzysobo/soboapptpl/widgets/platform.kt
+++ soboAppTpl/src/commonMain/kotlin/com/krzysobo/soboapptpl/widgets/platform.kt
@@ -1,7 +1,5 @@
 package com.krzysobo.soboapptpl.widgets
 
-//import com.krzysobo.kt0.getPlatform
-
 fun isPlatformAndroid(platformName: String): Boolean {
 //    return (getPlatform().name.contains("Android"))
     return (platformName.contains("Android"))
